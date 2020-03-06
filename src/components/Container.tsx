05b987f6943e68cd54f62de8bc541be3f02a78a7
Paweł Gdak <ppawcix@gmail.com>
2020-03-06 10:23:50 +0100

    Add Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import styled from 'styled-components';
+
+const StyledContainer = styled(Col)`
+    padding: 24px 0;
+`;
+
+export default function Container(props: { children: any }) {
+    return (
+        <Row justify="center">
+            <StyledContainer lg={18} md={20} xs={24} sm={20}>
+                {props.children}
+            </StyledContainer>
+        </Row>
+    );
+}
