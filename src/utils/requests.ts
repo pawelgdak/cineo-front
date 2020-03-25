1bc235e0ad569a48ad13cdf9d23ab4a269166085
Paweł Gdak <ppawcix@gmail.com>
2020-03-25 22:25:47 +0100

    Add possibility to simulate weak network connection for testing

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -53,44 +53,50 @@ export async function get(endpoint: string, options: any = {}): Promise<any> {
     if (process.env.REACT_APP_MOCK_REQUESTS) {
         try {
             const MOCK = await import(`../mocks/get${endpoint}`);
-            return {
+            const returnObject = {
                 status: 200,
                 message: 'OK',
                 data: MOCK.default,
             };
+
+            if (process.env.REACT_APP_SIMULATE_WEAK_NETWORK) {
+                await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 5 + 1) * 1000));
+            }
+
+            return returnObject;
         } catch (err) {
             throw new Error("Mock doesn't exists");
         }
-    }
+    } else {
+        let userToken;
 
-    let userToken;
+        if (options.useToken) {
+            // userToken = await AsyncStorage.getItem('jwt-token');
 
-    if (options.useToken) {
-        // userToken = await AsyncStorage.getItem('jwt-token');
-
-        if (!userToken) {
-            throw new Error('No user token');
-        }
-    }
-
-    try {
-        const API_RESPONSE = await axios.get(
-            `${process.env.apiServer}/${endpoint}`,
-            options.useToken
-                ? {
-                      headers: { Authorization: `Bearer ${userToken}` },
-                  }
-                : {},
-        );
-
-        if (API_RESPONSE.status === 200 && API_RESPONSE.data.success) {
-            return API_RESPONSE.data;
-        } else return false;
-    } catch (err) {
-        if (err.response && err.response.data) {
-            throw new Error(err.response.data.message);
+            if (!userToken) {
+                throw new Error('No user token');
+            }
         }
 
-        return false;
+        try {
+            const API_RESPONSE = await axios.get(
+                `${process.env.apiServer}/${endpoint}`,
+                options.useToken
+                    ? {
+                          headers: { Authorization: `Bearer ${userToken}` },
+                      }
+                    : {},
+            );
+
+            if (API_RESPONSE.status === 200 && API_RESPONSE.data.success) {
+                return API_RESPONSE.data;
+            } else return false;
+        } catch (err) {
+            if (err.response && err.response.data) {
+                throw new Error(err.response.data.message);
+            }
+
+            return false;
+        }
     }
 }
