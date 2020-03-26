70960ba75e938f9ac3847fa6aff8290fc9c988bb
Paweł Gdak <ppawcix@gmail.com>
2020-03-26 19:46:23 +0100

    Add get show mock

---
+++ src/mocks/getshow.ts
@@ -0,0 +1,20 @@
+import IShow from '../interfaces/IShow';
+
+export default {
+    date: 1584552600,
+    movie: {
+        id: 2,
+        title: 'Bad Boys 4 Life',
+        img: 'https://static01.nyt.com/images/2020/01/26/arts/26box/26box-videoSixteenByNineJumbo1600.jpg',
+        posterImg:
+            'https://m.media-amazon.com/images/M/MV5BMWU0MGYwZWQtMzcwYS00NWVhLTlkZTAtYWVjOTYwZTBhZTBiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
+        duration: 105,
+        description:
+            'Gdy z więzienia ucieka pałająca żądzą zemsty baronowa narkotykowego kartelu, bohaterowie stają przed największym wyzwaniem w swojej dotychczasowej karierze.',
+        genre: 'Sensacyjny',
+        cast: ['Will Smith', 'Martin Lawrence', 'Vanessa Hudgens', 'Alexander Ludwig', 'Charles Melton', 'Paola Nuñez'],
+        imdbRating: 7.1,
+        metacriticRating: 75,
+    },
+    hall: 1,
+} as IShow;
