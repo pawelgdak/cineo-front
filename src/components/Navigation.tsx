9283aaa26f55706426d79d016eede8b8853a8394
Paweł Gdak <ppawcix@gmail.com>
2020-03-26 18:55:43 +0100

    Add logout button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
 import AccountDrawer from './AccountModal';
+import { useGlobalState } from '../state';
+import { removeToken } from '../utils/token';
+import { Modal } from 'antd';
 
 const Container = styled.div`
     display: flex;
@@ -20,6 +23,8 @@ const NavLink = styled(Link)`
     padding: 8px 18px;
     margin: 0 12px;
     border-radius: 4px;
+
+    ${props => props.style}
 
     transition: all 0.2s;
 
@@ -50,6 +55,23 @@ export default function Navigation() {
     const location = useLocation();
     const [visible, setVisible] = useState(false);
 
+    const [user, setUser] = useGlobalState('user');
+
+    const handleLogout = () => {
+        Modal.confirm({
+            title: 'Jesteś pewny/a że chcesz się wylogować?',
+            content: '',
+            okText: 'Tak',
+            okType: 'danger',
+            cancelText: 'Nie',
+            onOk() {
+                removeToken();
+                setUser(null);
+            },
+            onCancel() {},
+        });
+    };
+
     return (
         <Container>
             <NavLink to="/" active={(location.pathname === '/').toString()}>
@@ -58,11 +80,22 @@ export default function Navigation() {
             <NavLink to="/seanse" active={(location.pathname === '/seanse').toString()}>
                 <NavItem>SEANSE</NavItem>
             </NavLink>
-            <NavLink to={location.pathname} onClick={() => setVisible(true)}>
+            <NavLink
+                style={{ marginRight: 0 }}
+                to={user ? '/user' : location.pathname}
+                onClick={() => !user && setVisible(true)}
+            >
                 <NavItem>
                     <UserOutlined />
                 </NavItem>
             </NavLink>
+            {user && (
+                <NavLink style={{ marginLeft: 0 }} to={location.pathname} onClick={() => handleLogout()}>
+                    <NavItem>
+                        <LogoutOutlined />
+                    </NavItem>
+                </NavLink>
+            )}
 
             <AccountDrawer setVisible={(value: boolean) => setVisible(value)} visible={visible} />
         </Container>
--- src/state.ts
+++ src/state.ts
@@ -2,5 +2,5 @@ import { createGlobalState } from 'react-hooks-global-state';
 import IUser from './interfaces/IUser';
 
 export const { useGlobalState } = createGlobalState({
-    user: {} as IUser,
+    user: null as IUser,
 });
