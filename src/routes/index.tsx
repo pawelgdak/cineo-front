756a2c7d28efa4bc520cda5abe59171f0b440c60
Paweł Gdak <ppawcix@gmail.com>
2020-04-05 21:06:24 +0200

    Add user auto-login on refresh

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,32 @@
 import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { getToken } from '../utils/token';
+import { getToken, removeToken } from '../utils/token';
 
 import Home from './home';
 import Showtimes from './showtimes';
 import Show from './show';
+import { get } from '../utils/requests';
+import { useGlobalState } from '../state';
 
 export default function Routes() {
+    const [user, setUser] = useGlobalState('user');
+
     // Check if token stored and log in if so
     useEffect(() => {
         (async () => {
             const token = await getToken();
 
-            console.log(token);
+            if (token) {
+                try {
+                    const API_RESPONSE = await get('users/me');
+
+                    if (API_RESPONSE) {
+                        setUser(API_RESPONSE);
+                    } else removeToken();
+                } catch (err) {
+                    removeToken();
+                }
+            }
         })();
     }, []);
 
