5ef92be26b4b725e588772b40ececef067722963
Paweł Gdak <ppawcix@gmail.com>
2020-04-05 22:04:03 +0200

    Add empty screen on user loading, fix permissions system

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { getToken, removeToken } from '../utils/token';
 
@@ -9,9 +9,11 @@ import Show from './show';
 import { get } from '../utils/requests';
 import { useGlobalState } from '../state';
 import Page404 from '../components/Page404';
+import Panel from './panel';
 
 export default function Routes() {
     const [user, setUser] = useGlobalState('user');
+    const [loading, setLoading] = useState(false);
 
     // Check if token stored and log in if so
     useEffect(() => {
@@ -19,26 +21,40 @@ export default function Routes() {
             const token = await getToken();
 
             if (token) {
+                setLoading(true);
                 try {
                     const API_RESPONSE = await get('users/me');
 
                     if (API_RESPONSE) {
                         setUser(API_RESPONSE);
-                    } else removeToken();
+                        setLoading(false);
+                    } else {
+                        removeToken();
+                        setLoading(false);
+                    }
                 } catch (err) {
                     removeToken();
+                    setLoading(false);
                 }
             }
         })();
     }, []);
 
-    const Auth = (props: { Render: any }) => {
-        const { Render } = props;
+    const Auth = (props: { Render: any; permission?: Number }) => {
+        const { Render, permission } = props;
 
         if (user && user.id) {
-            return <Render />;
+            if (typeof permission == 'undefined') {
+                return <Render />;
+            } else if (permission === user.permission) {
+                return <Render />;
+            } else return <Page404 />;
         } else return <Page404 />;
     };
+
+    if (loading) {
+        return <div />;
+    }
 
     return (
         <Switch>
@@ -50,6 +66,9 @@ export default function Routes() {
             </Route>
             <Route path="/user">
                 <Auth Render={User} />
+            </Route>
+            <Route path="/panel">
+                <Auth permission={0} Render={Panel} />
             </Route>
             <Route exact path="/">
                 <Home />
